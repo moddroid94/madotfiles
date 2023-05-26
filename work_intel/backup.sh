549b6161c6c78be3e3cdb9c78e045fa770711f1e
moddroid94 <ilsecco94@gmail.com>
2023-05-26 17:49:05 +0200

    updated config files added alpha backup script to laid a template of the file

---
+++ work_intel/backup.sh
@@ -0,0 +1,3 @@
+cp -r /home/sam/.config/hypr/ /home/sam/Documents/madotconfig/
+cp -r /home/sam/.config/waybar/ /home/sam/Documents/madotconfig/
+cp -r /home/sam/.config/variety/ /home/sam/Documents/madotconfig/
